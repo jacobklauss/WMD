5929a01bbbcb52b56f5087fdb9ef21aa1503786b
Jacob <jakieschneider13@gmail.com>
2018-02-02 17:47:44 +1100

    Refactored Project Sourcetree (reworked the inner workings) (your website is still complete, Marley)

---
+++ UwAmp/www/Register Screen/register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE.html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="register.css">
+</head>
+
+
+<body>
+    <form>
+        
+        <input name="Username" placeholder="Username">
+        <input type='Email' placeholder="Email" name='email'>
+        <input type="radio" name="robot"><label>I am a robot</label>
+        <input type="radio" name="robot"><label>Check this box to confirm</label>
+        <input type="radio" name="robot"><label>I am not a robot</label>
+
+
+    </form>
+</body>
+
+<html>
